fef6d440d0cb8b202f933c1ab5e69779102f14fb
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-05-03 20:50:39 -0300

    my goodness

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,20 +7,77 @@ const bcrypt = require('bcrypt')
 // @route GET /users
 // @acess Private
 const getAllUsers = asyncHandler(async (req, res) => {
-
+    const users = await User.find().select('-password').lean() //o select() não devolve o password
+    if (!users) { //leia-se "se: eu não tiver users"
+        return res.status(400).json({ message: 'No user Found'})
+    }
+    res.json(users)
 })
 
 // @desc Create new user
 // @route POST /users
 // @acess Private
 const createNewUser = asyncHandler(async (req, res) => {
+    const { username, password, roles } = req.body
+
+    // Confirm data
+    if (!username || !password || !Array.isArray(roles) || !roles.length) {
+        return res.status(400).json({ message: 'All fields are required'})
+    }
+
+    //check for duplicate
+    const duplicate = await User.findOne({ username }).lean().exec()
+
+    if (duplicate) { 
+        return res.status(409).json({ message: 'duplicate username'})
+    }
+
+    // hash password
+    const hashedPwd = await bcrypt.hash(password, 10) //salt rounds
+
+    const userObject = { username, "password": hashedPwd, roles}
+
+    // create and store new user
+    const user = await User.create(userObject)
+
+    if (user) { //created
+        res.status(201).json({ message: `new user ${username} created`})
+    } else {
+        res.status(400).json({ message: 'Invalid user data received'})
+    }
 })
 
 // @desc update a user
 // @route PATCH /users
 // @acess Private
 const updateUser = asyncHandler(async (req, res) => {
+    const { id, username, roles, active, password } = req.body
 
+    //confirm data
+    if(!id || !username || !Array.isArray(roles) || !roles.length || typeof active != 'boolean') {
+        return res.status(400).json({ message: 'All fields are required'})
+    }
+
+    const user = await User.findById(id).exec()
+
+    if(!user) {
+        return res.status(400).json({ message: 'User not found'})
+    }
+
+    //check for duplicate
+    const duplicate = await User.findOne({ username }).lean().exec()
+    //allow updates to the original user
+    if (!duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({ message: "Duplicate username"})
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+    if (password) {
+        //hash password
+    }
 })
 
 // @desc Delete a user
