8512e686074bad05b6119b65dbc134ee9d4443e6
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-07-01 18:12:55 -0300

    PQP

--- models/Note.js
+++ models/Note.js
@@ -1,36 +1,35 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 const AutoIncrement = require('mongoose-sequence')(mongoose)
-// para seguir uma sequencia, e sim é assim estranho mesmo
 
 const noteSchema = new mongoose.Schema(
-	{
-		user: {
-			type: mongoose.Schema.Types.ObjectId, //é um tipo de schema objectID
-			required: true,
-			ref: "User", //a qual schema? ao schema User se refere.
-		}, 
-		title: {
-			type: String,
-			required: true,
-		},
-		text: {
-			type: String,
-			required: true,
-		},
-		completed: {
-			type: Boolean,
-			default: false, //qnd criamos um note, ele deve vir como falso, pq queremos q se torne true
-		},
-	},
-	{
-		timestamps: true, //vai dar o tempo q foi criado
-	}
-);
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+)
 
 noteSchema.plugin(AutoIncrement, {
-    inc_field: 'ticket',      //vai criar um ticket field dentro do noteschema
+    inc_field: 'ticket',
     id: 'ticketNums',
     start_seq: 500
 })
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
