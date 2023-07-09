a9afef821eb24c6cb4ec6828f0490fb56b18e02e
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-07-09 14:51:15 -0300

    deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'https://technotes-7lz9.onrender.com',
-    'https://technotes.onrender.com'
+    'https://technotes.onrender.com',
+    'https://technotes-sdrb.onrender.com',
+    'https://technote-api-kuor.onrender.com'
 ]
 
 module.exports = allowedOrigins
